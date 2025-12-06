581a73f6bb3be0d0b4261feac5db835ab05fb6b4
kkhhjjoo <khjoo1203@gmail.com>
2025-12-07 02:21:33 +0900

    원시타입-모든 타입의 기본

---
+++ primitive-type/main.ts
@@ -0,0 +1,19 @@
+// string
+// number
+// boolean
+// null
+// undefined
+// ----------
+// symbol
+// bigint
+
+let a: boolean = true;
+a = false;
+
+let b: null = null;
+let c: undefined = undefined;
+
+function double(n:number):number {
+  return n * 2;
+}
+console.log(double(3));
