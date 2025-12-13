c795bed2b54c1de1c7744af6a820e749a3e3a92e
kkhhjjoo <khjoo1203@gmail.com>
2025-12-14 00:20:10 +0900

    유니온 타입의 함정

--- 06.union-type/main2.ts
+++ 06.union-type/main2.ts
@@ -28,13 +28,15 @@ getDate({ start: '2024-01-01', end: '2025-01-01' });
 
 //3. in
 type Track2 = {
+  type: 'track';
   title: string;
   releaseDate: string;
 }
 
 type Artist2 = {
+  type: 'artist';
   name: string;
-  releaseDate: string;
+  debutDate: string;
 }
 
 function getName(result: Track2 | Artist2) {
@@ -61,4 +63,33 @@ function printInfo(result: Track2 | Artist2) {
   } else if (isArtist(result)) {
     console.log(result.name);
   }
+}
+
+const result: Track2 | Artist2 = {
+  type: 'track',
+  title: 'hey',
+  releaseDate: '2025'
+}
+
+interface Radio { 
+  type: 'radio',
+  title: string,
+  numberOfSongs: number
+}
+
+type SearchResult2 = Track2 | Artist2 | Radio;
+
+function getTypeName(result: SearchResult2) {
+  if (result.type === 'track') return '트랙';
+  else if (result.type === 'artist') return '아티스트';
+    //radio타입을 지나친다
+  else if (result.type === 'radio') return '라디오';
+  else {
+    exhaustiveCheck(result);
+    return '결과';
+  }
+}
+
+function exhaustiveCheck(param: never) { 
+  throw new Error('에러');
 }
